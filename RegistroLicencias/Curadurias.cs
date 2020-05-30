bc68892e18ac097abc92b421cc7b74c20a5d1a69
fernastereo <fernandoecueto@gmail.com>
2020-05-29 19:50:33 -0500

    Agregada clase para manejo de datos de curadurias

---
+++ RegistroLicencias/Curadurias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroLicencias
+{
+    class Curadurias
+    {
+        public int id { get; set; }
+        public String Nombre { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+
+    }
+}
